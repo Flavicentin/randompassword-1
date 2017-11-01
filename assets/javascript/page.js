d646ea92c2238dd2d5876b32797f38fb76ee36ed
Joseph Schefer <joseph.schefer@gmail.com>
2017-11-01 16:52:08 -0400

    Added rolodex thingy

--- assets/javascript/page.js
+++ assets/javascript/page.js
@@ -3,7 +3,7 @@ $( document ).ready(function() {
   $( ".sortable" ).disableSelection();
   $('li').filter(":nth-child(1)").addClass("primary-option"); // Selects only the first element of the word lists
   clickDrag();
-
+  $("#right-col").drum({ panelCount: 8 });
 });
 
 function clickDrag() {
